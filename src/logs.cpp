71a5499b0f1bc2d93f7dce0150cc409562b81586
konstantin-kalenberg <konstantin.kalenberg@rapyuta-robotics.com>
2019-10-18 15:10:05 +0900

    added cpp file

---
+++ src/logs.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <stdlib.h>
+ 
+int main(int argc, char** argv) {
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+    {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+    
+    ros::init(argc,argv,"log_demo");
+    ros::NodeHandle nh;
+    ros::Rate loop_rate(0.5); // We create a Rate object of 2Hz
+    
+    while (ros::ok()) //Endless loop until Ctrl+c
+    {
+        ROS_DEBUG("This is a DEBUG message");
+        ROS_INFO("This is a INFO message");
+        ROS_WARN("This is a WARN message");
+        ROS_ERROR("This is a ERROR message");
+        ROS_FATAL("This is a FATAL message");
+        
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
+    
+    return ;
+}
